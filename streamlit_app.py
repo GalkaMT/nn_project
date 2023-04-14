1e9c45448ec21bcba7c4e21349fbf93d9fc42e93
GalkaMT <gtuznikova@gmail.com>
2023-04-14 14:00:03 +0300

    second try

---
+++ streamlit_app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision import transforms as T
+from torchvision.models import inception_v3
+from torchvision.models import Inception_V3_Weights
+
+
+# def load_file():
+#     uploaded_file = st.file_uploader( 'Upload your file', type=["jpg", "jpeg", "png"])
+#     if uploaded_file is not None:
+#         image_data = uploaded_file.getvalue()
+#         st.image(image_data)
+#         return Image.open(io.BytesIO(image_data))
+#     else:
+#         return None
+
+def load_image():
+    upload_file_img = st.file_uploader(label="Выберите изображение")
+    if upload_file_img:
+        img_data = upload_file_img.getvalue()
+        st.image(img_data)
+        img = Image.open(io.BytesIO(img_data))
+        preprocess = T.Compose([
+            T.Resize((299, 299)),
+            T.ToTensor()])
+        image_tensor = preprocess(img)
+        return image_tensor
+
+     
+
+st.title("Inception v.3 Classification Images")
+st.write('''Read more about this convolutional neural network [here](https://en.wikipedia.org/wiki/Inceptionv3') or [here](https://habr.com/ru/articles/302242/)''')
+
+img = load_image()
+
+res_button = st.button("Analyse image")
+
+# image transformation
+# trans = T.Compose([T.Resize((299,299)), T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),T.ToTensor()])
+# prepared_img=trans(img)
+
+# Initialize model with the best available weights
+weights = Inception_V3_Weights.DEFAULT
+model = inception_v3(weights=weights)
+model.eval()
+
+# Initialize the inference transforms
+preprocess = weights.transforms()
+
+# apply inference preprocessing transforms
+batch = preprocess(img).unsqueeze(0)
+
+
+prediction = model(batch).squeeze(0).softmax(0)
+class_id = prediction.argmax().item()
+score = prediction[class_id].item()
+category_name = weights.meta["categories"][class_id]
+st.write(f"{category_name}: {100 * score:.1f}%")
